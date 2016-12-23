900057cb3983aa59cdd1a523f6338672254d70c5
aoisensi <aoisensi@outlook.jp>
2016-12-23 18:56:48 +0900

    more strip!!

--- generate.sh
+++ generate.sh
@@ -1,7 +1,7 @@
 for size in 32 48 64 72 96 128 144 192 256 512
 do
-    convert -strip -geometry ${size}x${size} -filter box aoisensi.svg aoisensi${size}px.png
+    convert -strip -define png:exclude-chunk=tEXt,zTXt,tIME -geometry ${size}x${size} -filter box aoisensi.svg aoisensi${size}px.png
 done
 ln -f -s aoisensi32px.png aoisensi.png
 
-convert aoisensi.svg -define icon:auto-resize -filter box aoisensi.ico
+convert -strip aoisensi.svg -define icon:auto-resize -filter box aoisensi.ico
