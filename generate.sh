091b29492dc542e05a4a87f02b5bce4517e7a31a
aoisensi <aoisensi@outlook.jp>
2016-12-23 17:15:28 +0900

    added strip

--- generate.sh
+++ generate.sh
@@ -1,7 +1,7 @@
 for size in 32 48 64 72 96 128 144 192 256 512
 do
-    convert -geometry ${size}x${size} -filter box aoisensi.svg aoisensi${size}px.png
+    convert -strip -geometry ${size}x${size} -filter box aoisensi.svg aoisensi${size}px.png
 done
-ln aoisensi32px.png aoisensi.png
+ln -f -s aoisensi32px.png aoisensi.png
 
 convert aoisensi.svg -define icon:auto-resize -filter box aoisensi.ico
